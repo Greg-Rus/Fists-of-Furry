fefe5988b19fedaba3b52606b3c3099069970710
Greg-Rus <grzegorz.rus@gmail.com>
2020-10-24 22:26:04 +0200

    missed a config asset

--- Assets/_Scripts/Config/PlayerConfig.cs
+++ Assets/_Scripts/Config/PlayerConfig.cs
@@ -6,4 +6,7 @@ public class PlayerConfig : ScriptableObject
     public float AttackRange;
     public float WalkingSpeed;
     public float ChargingSpeed;
+    public float AttackAnimationSpeed;
+    public float AttackWindDownAnimationSpeed;
+    public float DefaultAnimationSpeed;
 }
